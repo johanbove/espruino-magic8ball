72569d5e65322ff9534c1b4e4c83a900cc5678a3
Johan Bové <me@johanbove.info>
2020-02-04 22:35:08 +0100

    Implemented as Bangle app

--- app.js
+++ app.js
@@ -1,39 +1,28 @@
-const appname = "jbm8b";
-require("Storage").write("+" + appname, {
-  "name": "Magic 8 Ball",
-  "description": "A simple fortune telling app",
-  "src": "-" + appname,
-  //"icon":"*"+appname,
-  "version": "1.1.0",
-  "type": "",
-  "tags": "game",
-});
-require("Storage").write("-" + appname, `
 const affirmative = [
-  'It is\\ncertain.',
-  'It is\\ndicededly\\nso.',
-  'Without\\na doubt.',
-  'Yes\\ndefinitely.',
-  'You may\\nrely\\non it.',
-  'As I see,\\nit yes.',
-  'Most\\nlikely.',
-  'Outlook\\ngood.',
+  'It is\ncertain.',
+  'It is\ndicededly\nso.',
+  'Without\na doubt.',
+  'Yes\ndefinitely.',
+  'You may\nrely\non it.',
+  'As I see,\nit yes.',
+  'Most\nlikely.',
+  'Outlook\ngood.',
   'Yes.',
-  'Signs point\\nto yes.'
+  'Signs point\nto yes.'
 ];
 const nonCommittal = [
-  'Reply hazy,\\ntry again.',
-  'Ask again\\nlater.',
-  'Better not\\ntell you\\nnow.',
-  'Cannot\\npredict\\nnow.',
-  'Concentrate\\nand\\nask again.'
+  'Reply hazy,\ntry again.',
+  'Ask again\nlater.',
+  'Better not\ntell you\nnow.',
+  'Cannot\npredict\nnow.',
+  'Concentrate\nand\nask again.'
 ];
 const negative = [
-  'Don\\'t\\ncount on it.',
-  'My reply\\nis no.',
-  'My sources\\nsay no.',
-  'Outlook\\nis not\\nso\\ngood.',
-  'Very\\ndoubtful.'
+  'Don\'t\ncount on it.',
+  'My reply\nis no.',
+  'My sources\nsay no.',
+  'Outlook\nis not\nso\ngood.',
+  'Very\ndoubtful.'
 ];
 
 const title = 'Magic 8 Ball';
@@ -71,16 +60,12 @@ function reply(button) {
 }
 
 function ask() {
-  draw('Ask me a\\nYes or No\\nquestion\\nand\\ntouch the\\nscreen');
+  draw('Ask me a\nYes or No\nquestion\nand\ntouch the\nscreen');
 }
 
 g.clear();
 
 Bangle.loadWidgets();
-Bangle.drawWidgets();
-
-// Start
-ask();
 
 // Event Handlers
 
@@ -91,4 +76,10 @@ setWatch(reply, BTN3, {repeat:true, edge:"falling"});
 
 // Back to launcher
 setWatch(Bangle.showLauncher, BTN2, {repeat:false, edge:"falling"});
-`);
+
+Bangle.on('lcdPower', (on) => {
+  if (on) {
+    Bangle.drawWidgets();
+    ask();
+  }
+});
